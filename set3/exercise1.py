fccd47f3e5b37218add03514e1b6122122737109
ZHONGHOIHUNG <zhonghoihung123@gmail.com>
2023-06-16 16:11:09 +1000

    set3 excercise1

--- set3/exercise1.py
+++ set3/exercise1.py
@@ -17,7 +17,13 @@ def loop_ranger(start, stop=None, step=1):
     Look up for how range() works in the python docs. You could  answer this
     with just the range function, but we'd like you to do it the long way.
     """
-    return None
+    numbers = []
+    middle_number = start
+
+    while middle_number < stop:
+        numbers.append(middle_number)
+        middle_number += step
+    return numbers
 
 
 def two_step_ranger(start, stop):
@@ -28,7 +34,13 @@ def two_step_ranger(start, stop):
 
     You can either reuse loop_ranger, or the range function that in the standard library
     """
-    return None
+    numbers = []
+    middle_number = start
+
+    while middle_number < stop:
+        numbers.append(middle_number)
+        middle_number += 2
+    return numbers
 
 
 def stubborn_asker(low, high):
@@ -39,7 +51,15 @@ def stubborn_asker(low, high):
 
     Look up the docs for a function called "input"
     """
-    return None
+    while True:
+        number = input("Enter a number between {} and {}: ".format(low, high))
+        try:
+            number = int(number)
+            if low <= number <= high:
+                return number
+        except ValueError:
+            pass 
+        print("wrong number")
 
 
 def not_number_rejector(message):
@@ -49,7 +69,13 @@ def not_number_rejector(message):
     (e.g. "cow", "six", "8!") then throw it out and ask for an actual number.
     When you do get a number, return it.
     """
-    return None
+    while True:
+        number = input(message)
+        try:
+            number = float(number)  # Convert input to float
+            return number
+        except ValueError:
+            print("try again with an actual number")
 
 
 def super_asker(low, high):
@@ -58,7 +84,14 @@ def super_asker(low, high):
     Combine what you learnt from stubborn_asker and not_number_rejector
     to make a function that does it all!
     """
-    return None
+    while True:
+        number = input("Enter a number between {} and {}: ".format(low, high))
+        try:
+            number = int(number)
+            if low <= number <= high:
+                return number
+        except ValueError:
+            print("Please provide an actual number between low and high")
 
 
 if __name__ == "__main__":
